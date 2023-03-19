45a97b978e0b57cc9152d0f8dbf560765343768a
sangHyun <public.winverse@gmail.com>
2023-03-19 11:35:58 +0900

    Fix: Change swagger document title

--- src/swagger.ts
+++ src/swagger.ts
@@ -4,7 +4,7 @@ import { NestFastifyApplication } from "@nestjs/platform-fastify";
 
 export const swagger = async (fastify: NestFastifyApplication) => {
   const swaggerConfig = new DocumentBuilder()
-    .setTitle("Salon-time-slot")
+    .setTitle("Salon Scheduler")
     .setVersion("1.0")
     .build();
 
